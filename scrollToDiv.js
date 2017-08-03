b044ac5e9dd11f0394e9acf4ce568bf7a4ced338
Jason Shao <jason@Jasons-MacBook-Pro.local>
2017-08-02 21:04:57 -0700

    changed scrolling function to multiscroll

--- scrollToDiv.js
+++ scrollToDiv.js
@@ -1,27 +1,32 @@
-let scrollToDiv = function() {
-      let scrollDivs = ['#page-top', '.bg-primary', '.bg-secondary', '.bg-contact'];
-      //put divs to scroll to in order here
-      let currentDivState = 0;
-      $(window).bind('mousewheel', function(event) {
-      if (event.originalEvent.wheelDelta >= 0) {
-        if(currentDivState > 0) {
-          currentDivState -= 1;
-          $('html, body').animate({
-            scrollTop: $(scrollDivs[currentDivState]).offset().top
-          }, 1000, 'easeInOutExpo');
-          event.preventDefault();
-          //change animation speed here based on height later
-        }
+let scrollToDiv = function () {
+  let scrollDivs = ['#page-top', '.bg-primary', '.bg-secondary', '.bg-contact'];
+  //replace divs you want to scroll to in scrollDivs array in order here. 
+  let currentDivState = 0;
+  let waitState = false;
+  $(window).bind('mousewheel', function (event) {
+    event.preventDefault();
+    setTimeout(function () { waitState = true }, 1500);
+    if (event.originalEvent.wheelDelta >= 0 && waitState) {
+      if (currentDivState > 0) {
+        waitState = false;
+        currentDivState -= 1;
+        $('html, body').animate({
+          scrollTop: $(scrollDivs[currentDivState]).offset().top
+        }, 750, 'easeInOutExpo');
       }
-      else {
-        if(currentDivState < scrollDivs.length - 1) {
+    }
+    else {
+      if (event.originalEvent.wheelDelta < 0 && waitState) {
+        if (currentDivState < scrollDivs.length - 1) {
+          waitState = false;
           currentDivState += 1;
           $('html, body').animate({
             scrollTop: $(scrollDivs[currentDivState]).offset().top
-          }, 1000, 'easeInOutExpo');
-          event.preventDefault();
+          }, 750, 'easeInOutExpo');
         }
       }
-    });
-  };
-  scrollToDiv();
+    }
+  });
+
+};
+scrollToDiv();
